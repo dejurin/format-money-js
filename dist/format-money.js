6e77a2791c64c46e9045817144ca7e7725dfccd9
Yurii Derevych <gkhelloworld@gmail.com>
2020-01-26 00:35:50 +0200

    0.1.7

--- dist/format-money.js
+++ dist/format-money.js
@@ -1,12 +1,14 @@
+"use strict";
 /*!
- * format-money-js v0.1.6
+ * format-money-js v0.1.7
  * (c) 2020 Yurii Derevych
  * Released under the BSD-2-Clause License.
  */
-export class FormatMoney {
+Object.defineProperty(exports, "__esModule", { value: true });
+class FormatMoney {
     constructor(options) {
         this.options = options;
-        this.version = '0.1.6';
+        this.version = '0.1.7';
         this.defaults = {
             grouping: true,
             separator: ',',
@@ -39,4 +41,5 @@ export class FormatMoney {
         this.options = Object.assign(Object.assign({}, this.defaults), options);
     }
 }
+exports.FormatMoney = FormatMoney;
 //# sourceMappingURL=format-money.js.map
