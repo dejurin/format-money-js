f3dd7b154c365d4d6f2c7066cf71668e1241fb0e
Yurii Derevych <gkhelloworld@gmail.com>
2020-01-27 10:23:28 +0200

    1.1.3

--- src/format-money.ts
+++ src/format-money.ts
@@ -1,5 +1,5 @@
 /*!
- * format-money-js v1.1.2
+ * format-money-js v1.1.3
  * (c) 2020 Yurii Derevych
  * Released under the BSD-2-Clause License.
  */
@@ -15,7 +15,7 @@ export interface FormatMoneyOptions { // (default)
 
 export class FormatMoney {
 
-  version = '1.1.2';
+  version = '1.1.3';
   private defaults: FormatMoneyOptions = {
     grouping: true,
     separator: ',',
