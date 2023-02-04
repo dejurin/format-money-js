6180f9e868d2679be911d6a9b650a6b32c2d9829
YURII D <gkhelloworld@gmail.com>
2023-02-05 01:38:17 +0200

    1.6.1

--- src/format-money.ts
+++ src/format-money.ts
@@ -1,5 +1,5 @@
 /*!
- * format-money-js v1.6.0
+ * format-money-js v1.6.1
  * (c) 2020-2023 Yurii Derevych
  * URL: https://github.com/dejurin/format-money-js
  * Sponsored:
@@ -9,7 +9,7 @@
  */
 
 export interface FormatMoneyOptions { // (default)
-  grouping?: boolean; // Example: 1,000 vs 1000 (true)
+  grouping?: boolean; // Example: 1,000 vs 1000 (true)tsc --version
   separator?: string; // Grouping separator (,)
   decimalPoint?: string; // Sets the separator for the decimal point.
   decimals?: number; // Sets the number of decimal points.
@@ -28,7 +28,7 @@ export interface FormatMoneyParse { // Parse
 }
 
 export class FormatMoney {
-version = '1.6.0';
+version = '1.6.1';
 
 private defaults: FormatMoneyOptions = {
   grouping: true,
